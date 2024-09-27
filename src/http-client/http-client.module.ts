efb69957c562cd788eadc4d331aec47af73e993f
Hafsah <h.jilanee@rewaatech.com>
2024-09-27 11:42:20 +0500

    configurable module builder

---
+++ src/http-client/http-client.module.ts
@@ -0,0 +1,13 @@
+import { Inject, Module } from '@nestjs/common';
+import {
+  ConfigurableModuleClass,
+  HTTP_MODULE_OPTIONS,
+} from './http-client.module-definition';
+
+@Module({})
+export class HttpClientModule extends ConfigurableModuleClass {
+  constructor(@Inject(HTTP_MODULE_OPTIONS) private options) {
+    console.log(options);
+    super();
+  }
+}
